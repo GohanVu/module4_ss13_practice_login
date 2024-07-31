782d41d7e6b2424c9b420b0a43c7c9bcede7ed47
GohanVu <hoangvhdata@gmail.com>
2024-07-31 08:22:01 +0700

    update

---
+++ src/main/java/com/codegym/practice_login/PracticeLoginApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.practice_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/practice_login/PracticeLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.practice_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
